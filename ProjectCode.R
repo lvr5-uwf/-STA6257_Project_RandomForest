e4cdd5d930c2a19fd6a59e904a09d6103c8aa02d
lvr5-uwf <lvr5@students.uwf.edu>
2023-09-30 15:50:56 -0500

    added dataset files and R code file for testing changes before loading into the quarto file

---
+++ ProjectCode.R
@@ -0,0 +1,10 @@
+#this file is for coding and troubleshooting before putting into blocks on the qmd file
+
+library(tidyverse)
+
+#read the full dataset into a dataframe 
+df <- read_csv("diabetes_012_health_indicators_BRFSS2015.zip")
+
+#colnames(df)
+#summarize(df)
+
