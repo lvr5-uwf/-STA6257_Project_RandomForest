dd97572c0c422f93ab8a1bd87310d96a2eada271
lvr5-uwf <lvr5@students.uwf.edu>
2023-10-10 20:40:11 -0500

    added summary data in gtsummary format, removed r summary line, labeled code blocks, removed commented code block from original project, moved statistical modeling section header above code for decision trees

--- ProjectCode.R
+++ ProjectCode.R
@@ -4,6 +4,9 @@ library(tidyverse)
 #library(dplyr)
 library(rpart)
 library(rpart.plot)
+library(gtsummary)
+library(Hmisc)
+library(corrplot)
 
 randseed <- 123456
 set.seed(randseed)
@@ -23,7 +26,7 @@ coltypes <- c("f",#Diabetes_012
               "f",#HvyAlcoholConsump
               "f",#AnyHealthcare
               "f",#NoDocbcCost
-              "n",#GenHlth
+              "f",#GenHlth
               "n",#MentHlth
               "n",#PhysHlth
               "f",#DiffWalk
@@ -44,9 +47,26 @@ df <- read_csv("diabetes_binary_5050split_health_indicators_BRFSS2015.zip", col_
 #colnames(df)
 #summary(df)
 
+#print gt summary
+smr <-
+  tbl_summary(
+    df,
+    include = c("Diabetes_binary", "HighBP", "HighChol","CholCheck", "BMI", "Smoker", "Stroke", "HeartDiseaseorAttack", "PhysActivity", "Fruits", "Veggies","HvyAlcoholConsump", "AnyHealthcare", "NoDocbcCost", "GenHlth", "MentHlth", "PhysHlth", "DiffWalk","Sex", "Age", "Education", "Income"),
+    type = list(BMI ~ "continuous2", MentHlth  ~ "continuous2", PhysHlth  ~ "continuous2"),
+    statistic = all_continuous() ~ c("{median} ({p25}, {p75})", "{min}, {max}")
+  ) %>%
+  add_n() %>% # add column with total number of non-missing observations
+  bold_labels()
+smr
+
+#reload data with all columns as numeric (the default)
+# df.2 <- read_csv("diabetes_binary_5050split_health_indicators_BRFSS2015.zip")
+# df.corr <- rcorr(as.matrix(df.2))
+# corrplot(df.corr$r)
+# hist.data.frame(df.corr)
+
+
 #https://www.guru99.com/r-decision-trees.html
-
-
 #create this function variable so it is easier to change it as needed
 model_fn <- Diabetes_binary ~ HighBP + GenHlth + BMI + HighChol
 
@@ -57,4 +77,6 @@ rpart.plot(dt_model, box.palette = "Greens", main="Decision Tree", extra = 106)
 
 #older plot 
 prp(dt_model, faclen=0, cex=0.8, type=0, extra=106, main="Decision Tree")
+
+
 
